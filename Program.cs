a47f4e2390bfd66ee32906954ffb3894a3ef1ec4
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-07 23:20:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ReverseArray
+{
+   public class ArrayReverse
+    {
+    
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("enter the size of the array:");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("enter array elements:");
+           for(int i = 0; i < arr.Length ; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+               
+            }
+            Console.WriteLine("reverse elements are:");
+           for(int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            
+        }
+    }
+}
